a3547e7c70c07c33dc84f99168e35e352a417c72
Parkseonho <tjsgh2103@gmail.com>
2022-08-29 10:18:49 +0900

    JWT를 위한 강제 로그인

--- src/main/java/com/example/jwtserver_forlecture/config/SecurityConfig.java
+++ src/main/java/com/example/jwtserver_forlecture/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.jwtserver_forlecture.config;
 
+import com.example.jwtserver_forlecture.config.jwt.JwtAuthenticationFilter;
 import com.example.jwtserver_forlecture.filter.MyFilter1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
@@ -36,5 +39,14 @@ public class SecurityConfig{
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(new JwtAuthenticationFilter(authenticationManager));
+
+        }
     }
 }
